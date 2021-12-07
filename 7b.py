3790619b7f624895b78a16b2921105acc76b3a9e
Vincent-vL <95688845+Vincent-vL@users.noreply.github.com>
2021-12-07 22:26:22 +0100

    Update 7b.py

--- 7b.py
+++ 7b.py
@@ -4,6 +4,6 @@ p = np.int32(np.genfromtxt('input7.txt',delimiter=','))
 cost = np.inf
 for i in range(min(p), max(p) + 1):
    n = np.abs(p-i)
-   cost = np.minimum(cost, int(sum(n * (n + 1) / 2)))
+   cost = np.minimum(cost, sum(n * (n + 1) / 2))
 
 print(int(cost))
