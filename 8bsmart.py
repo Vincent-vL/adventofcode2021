9eb4998a4d6aa209d302a9c1a07745f556cdfac4
Vincent-vL <95688845+Vincent-vL@users.noreply.github.com>
2021-12-08 17:09:13 +0100

    Smarter everyday :)

--- 8bsmart.py
+++ 8bsmart.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 displays = np.loadtxt('input8.txt', dtype='str', delimiter=' ')
-nlines, ndigits = displays.shape
-defaultdigits = ['acdefg', 'eg', 'abcef', 'abceg', 'bdeg', 'abcdg', 'abcdfg', 'aeg', 'abcdefg', 'abcdeg'] #0, 1, 2, ..., 9
-weights = np.int32([np.char.count(defaultdigits, chr(digit)) for digit in range(ord('a'), ord('h'))])
-hist = np.sum(weights, 1)
-ref_ids = [sum(weights[:, j] * hist.T) for j in range(0, 10)]
+
+alldigits = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+alldisplays = ['acdefg', 'eg', 'abcef', 'abceg', 'bdeg', 'abcdg', 'abcdfg', 'aeg', 'abcdefg', 'abcdeg'] # = 0, 1, .., 9
+allsegments = np.int32([np.char.count(alldisplays, digit) for digit in alldigits])
+histogram = np.sum(allsegments, 1)
+digit_ids = [sum(allsegments[:, j] * histogram.T) for j in range(0, 10)]
+
 s = 0
+nlines, ndigits = displays.shape
 for i in range(0, nlines):
    # compile lookup for this iteration
-   h = np.int32([np.char.count(displays[i, 0:10], chr(digit)) for digit in range(ord('a'), ord('h'))])
-   hist = np.sum(h, 1)
+   isegments = np.int32([np.char.count(displays[i, 0:10], digit) for digit in alldigits])
+   ihistogram = np.sum(isegments, 1)
    # decode and add value for each of the four digits
    for j in range(0, 4):
-      weights = np.int32([np.char.count(displays[i, 11+j], chr(digit)) for digit in range(ord('a'), ord('h'))])
-      id = sum(hist*weights)
-      n = np.argwhere(ref_ids == id)
-      s = s + n * 10 ** (3 - j)
+      jsegments = np.int32([np.char.count(displays[i, 11+j], digit)  for digit in alldigits])
+      id = sum(ihistogram*jsegments)
+      k = int(np.argwhere(digit_ids == id))
+      s = s + k * 10 ** (3 - j)
 
 print(s)
 
